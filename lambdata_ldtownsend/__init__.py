a8dd0ea530178bd6401f4fb630a53c655cc3057d
Luke Townsend <ldtownsend@users.noreply.github.com>
2019-09-30 17:31:45 -0500

    added some proof of concept utility functions

---
+++ lambdata_ldtownsend/__init__.py
@@ -0,0 +1,12 @@
+"""
+lambdata - a collection of data science helper functions
+"""
+
+import pandas as pd
+import numpy as np
+
+#sample code
+
+# Using all caps because these are "global variables", that is a common convention
+ONES = pd.DataFrame(np.ones(10))
+ZEROS = pd.DataFrame(np.zeros(50))
