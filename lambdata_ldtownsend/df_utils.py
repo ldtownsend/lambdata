e7450508725c2719c43a63ad2a28b1d178294a2e
Luke Townsend <ldtownsend@users.noreply.github.com>
2019-10-03 15:53:04 -0500

    with updated classes

--- lambdata_ldtownsend/df_utils.py
+++ lambdata_ldtownsend/df_utils.py
@@ -1,26 +1,53 @@
 """
-utility functions for working with DataFrames
+Utility functions for working with the Energy Information Agency's dataset
 """
 
-import pandas
+import pandas as pd
+import numpy as np
 
-TEST_DF = pandas.DataFrame([1,2,3])
+class Datetimes:
 
-def boxplot(x,y,dataframe):
-    import matplotlib.pyplot as plt
-    %matplotlib inline
-    return dataframe.boxplot(column="y",by="x")
+    def __init__(self, data):
+        """
+        takes in a DataFrame
+        """
+        self.data = data
+
+    def to_datetime(self, time_feats):
+        """
+        df is the DataFrame
+        time_feats is a list of the features to be changed to datetime format
+        """
+        for col in self.data:
+            if col in time_feats:
+                self.data[col] = pd.to_datatime(self.data[col])
 
 
-# Function for creating any time range
-def Prior_Period_YoY_Change(df_column, prior_period_length=1):
+    def dt_differences(self, dt_dif_name, dt1, dt2):
+        """
+        dt_dif_name enter a string for the name of the new feature
+        dt1 is the datetime feature you want to treat as the starting point
+        dt2 is the datetime feature you want to treat as the ending point
+        """
+        df1 = self.data.copy()
+        df1[dt_dif_name] = df1[dt1] - df1[dt2]
+        return df1[dt_dif_name]
 
-    numerator = 0
-    for i in range(prior_period_length + 1):
-        numerator += df_column.shift(-i)
 
-    denominator = 0
-    for i in range(prior_period_length + 1):
-        denominator += df_column.shift(-i - 12)
 
-    return (1 - (numerator / denominator)) 
+class Project_Energy:
+
+    def Prior_Period_YoY_Change(df_column, prior_period_length=1):
+        """
+        A calculation for finding the year-over-year growth for some feature,
+        over a certain period length, in a dataset with monthly frequency.
+        """
+        numerator = 0
+        for i in range(prior_period_length + 1):
+            numerator += df_column.shift(-i)
+
+        denominator = 0
+        for i in range(prior_period_length + 1):
+            denominator += df_column.shift(-i - 12)
+
+        return (1 - (numerator / denominator))
